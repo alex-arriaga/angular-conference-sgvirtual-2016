fd0338a86d87a657bcd1c8d3e5c0e827cf493ba7
Alex Arriaga <alex.arriaga.m@gmail.com>
2016-10-23 15:31:32 -0500

    Add example of how to create an event (post request)

--- app/services/events.service.ts
+++ app/services/events.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
 import { Event } from "./../models/event.model";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
 import { Observable }  from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 @Injectable()
 export class EventsService {
 
   baseURLPort: string = "3000";
   baseURL: string = "http://cosasextraordinarias.com";
-  baseUrlAPI : string = "";
+  baseUrlAPI: string = "";
 
   constructor(private _http: Http) {
     this.baseUrlAPI = `${this.baseURL}:${this.baseURLPort}`;
@@ -21,11 +22,25 @@ export class EventsService {
     return this._http.get(serviceURL).map((response: Response) => {
       // Fix the URL for images
       let items = response.json();
-      items.forEach((event : Event) => {
+      items.forEach((event: Event) => {
         event.thumbnail = `${this.baseURL}/api-images/${event.thumbnail.replace("/img/", "")}`;
       });
       console.log("-- getEvents() > Response from external service: %o", items);
       return items;
     });
+  }
+
+  public createEvent(event: Event): Observable<Response> {
+    let serviceURL: string = `${this.baseUrlAPI}/events`;
+
+    // let bodyString = JSON.stringify(event); // Stringify payload
+    let headers = new Headers({'Content-Type': 'application/json'}); // Set content type to JSON
+    let options = new RequestOptions({headers: headers}); // Create a request option
+
+    console.log(">> createEvent()", event);
+    return this._http.post(serviceURL, event, options).map((response: Response) => {
+      return response.json();
+    }).catch((error: any) => Observable.throw(error.json().error || 'Server error')); // Errors if any
+
   }
 }
--- app/widgets/content-tiles/content-tiles.component.ts
+++ app/widgets/content-tiles/content-tiles.component.ts
@@ -16,6 +16,34 @@ export class ContentTilesComponent implements OnInit {
   }
 
   public ngOnInit() {
+    this.getEvents();
+  }
+
+  // Don't confuse the $event with OUR class/model "EventItem"
+  // $event is a regular ECMAScript "Event"
+  public onCreateEvent(event: Event) {
+    event.preventDefault();
+
+    let newEventItem: EventItem = {
+      topics: "sports",
+      thumbnail: "/img/s-1.jpeg",
+      url: "index.html",
+      title: "Example of an event created programatically",
+      summary: "Georgetown at Butler. Georgetown's D'Vauntes Smith-Rivera squares off with Butler's Kellen Dunham in a battle of senior guards. The Hoyas took two of three meetings with the Bulldogs last season."
+    };
+
+    this._eventsService.createEvent(newEventItem).subscribe(data => {
+      console.log(data);
+      this.getEvents();
+    }, err => {
+      console.error(err);
+    }, () => {
+
+    });
+
+  }
+
+  public getEvents() {
     this._eventsService.getEvents().subscribe((data) => {
         this.events = data;
       },
@@ -27,5 +55,4 @@ export class ContentTilesComponent implements OnInit {
       }
     );
   }
-
 }
