08a8fe30aedaf4078094d2b08e0c90b3c603e0bc
Alex Arriaga <alex.arriaga.m@gmail.com>
2016-10-23 13:01:34 -0500

    Add Content Tiles Component to the application

---
+++ app/widgets/content-tiles/content-tiles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+@Component({
+  moduleId: module.id,
+  selector: "app-content-tiles",
+  templateUrl: "content-tiles.component.html",
+  styleUrls: ["content-tiles.component.css"]
+})
+export class ContentTilesComponent {
+
+}
