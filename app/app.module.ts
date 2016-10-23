9d2b1eec4f8b50a498a086044fee102f98ad1fde
Alex Arriaga <alex.arriaga.m@gmail.com>
2016-10-23 13:01:16 -0500

    Add Header Component to the application

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HeaderComponent } from "./common/layout/header.component";
 import { FooterComponent } from "./common/layout/footer.component";
 
 @NgModule({
