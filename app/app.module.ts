352e129b8537fe425c4dd9b556ced0c31702425f
Alex Arriaga <alex.arriaga.m@gmail.com>
2016-10-23 13:00:55 -0500

    Add FooterComponent to the application

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { FooterComponent } from "./common/layout/footer.component";
 
 @NgModule({
   imports: [ BrowserModule ],
---
+++ app/common/layout/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+@Component({
+  moduleId: module.id,
+  selector: "app-footer",
+  templateUrl: "footer.component.html",
+  styles: [
+    `
+      footer {
+        background-color: #efefef;
+        min-height: 50px;
+      }
+    `
+  ]
+})
+export class FooterComponent {
+
+}
